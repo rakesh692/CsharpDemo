8a7d93dd9403d69e0e22d693fa6f3758371c0f9a
Rakesh Tripathi <rakesh.tripathi15@gmail.com>
2024-07-10 11:27:58 +0100

    Initial Build

---
+++ AdvancedC#Demo/AdvancedC#Demo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedC_Demo
+{
+    public delegate void PrintDelgate(string message);
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var hello = new Hello();
+            PrintDelgate del = new PrintDelgate(hello.PrintHello);
+            del("Hello I am called from delegate!!");
+        }
+    }
+
+    class Hello
+    {        
+        public void PrintHello(string text)
+        {
+            Console.WriteLine(text);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ AdvancedC#Demo/AdvancedC#Demo/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AdvancedC#Demo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("AdvancedC#Demo")]
+[assembly: AssemblyCopyright("Copyright © HP 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1dea7bd2-4228-48b9-bc52-dcae4f92767d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
